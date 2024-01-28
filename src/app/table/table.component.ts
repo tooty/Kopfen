10290fdcb7268f61a2a98634aa2689ab193431a1
Thomas <ttichy@hotmail.de>
2024-01-28 17:37:34 +0100

    dark theme

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,45 @@
+import {CommonModule} from '@angular/common';
+import {  Component } from '@angular/core';
+import { Player } from '../player';
+import { Game } from '../game';
+import { StateService } from '../state.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.css'
+})
+export class TableComponent {
+  playersOver: Player[] = []
+  games: Game[] = []
+  sum: {p: Player, sum: number[]}[] = []
+
+  constructor(
+    private stateService: StateService,
+    private router: Router,
+  ) {
+    this.stateService.players$.subscribe((data) => {
+      this.playersOver = JSON.parse(JSON.stringify(data))
+    })
+    this.stateService.games$.subscribe((data) => {
+      this.games = data.sort((a,b)=> b.time-a.time)
+      this.sum = stateService.gameSum()
+    })
+  }
+
+  navigate(route: String){
+    this.router.navigate([route])
+  }
+
+  metchAmount(player: Player, game: Game): number {
+    let match = game.players.find(p => player.id == p.id)
+    if (match != null){
+      return match.cost
+    } else {
+      return 0
+    }
+  }
+}
