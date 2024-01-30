5b4e282062fcb12c0a5e5f3e466a5ac5fd524054
Thomas <ttichy@hotmail.de>
2024-01-30 09:06:55 +0100

    ui structure rebuild

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,77 +1,67 @@
 import { Component, ViewChild } from '@angular/core';
-import {NgChartsModule, BaseChartDirective } from 'ng2-charts';
-import {StateService} from '../state.service';
+import { NgChartsModule, BaseChartDirective } from 'ng2-charts';
+import { StateService } from '../state.service';
 import { Game } from '../game';
-import { ChartType,ChartConfiguration,} from 'chart.js';
-import {Player} from '../player';
+import { ChartType, ChartConfiguration } from 'chart.js';
+import { Player } from '../player';
+import 'chartjs-adapter-date-fns'
+import { de } from 'date-fns/locale'
 
 @Component({
   selector: 'app-graph',
   standalone: true,
   imports: [NgChartsModule],
   templateUrl: './graph.component.html',
-  styleUrl: './graph.component.css'
+  styleUrl: './graph.component.css',
 })
 export class GraphComponent {
-  sum: {p: Player, sum: number[]}[] = []
-  graphData: any
-  barChartOptions: any
-
-
-  constructor(
-    private stateService:StateService
-  ){
-    this.sum = stateService.gameSum()
-    this.stateService.games$.subscribe((data)=>{
-      this.sum = stateService.gameSum()
-      this.randomize()
-    })
-  }
-
-  lineChartData: ChartConfiguration['data'] = {
-    datasets: [
-    ]
-  };
-
-  public lineChartOptions: ChartConfiguration['options'] = {
+  sum: { p: Player; sum: number[] }[] = [];
+  games: Game[]=[]
+  graphData: any;
+  barChartOptions: any;
+  lineChartData: ChartConfiguration['data'] = { datasets: [] };
+  lineChartOptions: ChartConfiguration['options'] = {
     elements: {
       line: {
         tension: 0.3,
       },
-      point: {
-        pointStyle: 'line'
-      }
     },
     scales: {
       y: {
         position: 'right',
       },
     },
-
   };
-
   lineChartType: ChartType = 'line';
 
   @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+  constructor(private stateService: StateService) {
+    this.sum = stateService.gameSum();
+    this.stateService.games$.subscribe((data) => {
+      this.sum = stateService.gameSum();
+      this.games = data
+      this.buildData();
+    });
+  }
 
-  public randomize(): void {
-    let label: number[] = []
-    this.sum.forEach((x,i) => {
+  buildData(): void {
+    let label: number[] = [];
+    this.sum.forEach((x, i) => {
       this.lineChartData.datasets[i] = {
         data: x.sum,
         label: x.p.name.toString(),
         fill: 'origin',
-      }
+      };
       let arr = [];
-      for (let i = 1; i < x.sum.length+1; i++) {
-            arr.push(i);
+      for (let i = 1; i < x.sum.length + 1; i++) {
+        arr.push(i);
       }
-      if (arr.length != label.length && label.length != 0){
-        console.error("corrupted Data")
+      if (arr.length != label.length && label.length != 0) {
+        console.error('corrupted Data');
       }
-      label = arr
-    })
-    this.lineChartData.labels = label
+      label = arr;
+    });
+    this.lineChartData.labels = this.games.map((x,i) => i+1);
 
     this.chart?.update();
   }
