ab95a6778e37aa9e4c262157e4848ba80463dfd7
Thomas Tichy <ttichy@hotmail.de>
2024-02-22 10:56:27 +0100

    new

---
+++ src/app/controler.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import {HttpService} from './http.service';
+import {StateService} from './state.service';
+import { Player, Game } from './interfaces';
+import { Observable, interval, repeat } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ControlerService {
+
+  constructor(
+    private stateService: StateService,
+    private httpService: HttpService
+  ) {
+    const syncLoop = new Observable<null>()
+    syncLoop.pipe((x)=>{this.queueUnsynced();return x})
+    syncLoop.pipe(repeat({delay: 3000}))
+  }
+
+  addGame(ng: Game){
+    this.stateService.addGame(ng).then(()=>{
+      if (!ng.synced) {
+        this.pushGameToServer(ng)
+      }
+    })
+  }
+
+  addPlayer(np: Player){
+    this.stateService.addPlayer(np).then(()=>{
+      if (!np.synced) {
+        this.pushPlayerToServer(np)
+      }
+    })
+  }
+
+  pushGameToServer(g: Game){
+    const obs = this.httpService.pushGame({content: g,URL:'game'})
+    obs.subscribe(
+      {
+        complete: ()=> {
+          this.stateService.toggleGameSync(g.time,true)
+        }
+      }
+    )
+  }
+
+  pushPlayerToServer(g: Player){
+    const obs = this.httpService.pushGame({content: g,URL:'game'})
+    obs.subscribe(
+      {
+        complete: ()=> {
+          this.stateService.togglePlayerSync(g.id,true)
+        }
+      }
+    )
+  }
+
+  loadServerGames(s: Date, e: Date){
+    this.httpService.getGames(s.getUTCMilliseconds(), e.getUTCMilliseconds())
+      .subscribe((data) => {
+        data.map((x) => {
+          this.stateService.addGame(x, false);
+        });
+      });
+  }
+
+  queueUnsynced(){
+    this.stateService.getUnsyncedPlayers().map((x)=>{
+      this.pushPlayerToServer(x)
+    })
+    this.stateService.getUnsyncedGames().map((x)=>{
+      this.pushGameToServer(x)
+    })
+  }
+}
