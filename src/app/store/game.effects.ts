af7633ed56d9bb9d4118c01ed8ca25cc8cdea616
Thomas Tichy <ttichy@hotmail.de>
2024-03-07 09:25:41 +0100

    neu

--- src/app/store/game.effects.ts
+++ src/app/store/game.effects.ts
@@ -101,15 +101,14 @@ export class GameEffects {
   ))
 
   handlePullResponse$ = createEffect(() => this.actions$.pipe(
-        tap(()=>console.log("here")),
     ofType(GameAction.handlePullResponse),
     concatMap((game) => from(this.indexDbService.saveGame(game))
       .pipe(
-        tap(()=>console.log("here")),
         mergeMap(() => from([
           PlayerActin.newHttpPulledGame({ payload: game.involved }),
           GameAction.storeStore(game)
         ])),
+        tap((o)=>console.log(0)),
         catchError((e) => of({ type: '[indexDBService] Save Game Error', e }))
       )
     )
