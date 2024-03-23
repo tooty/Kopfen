5e1f9ec892171bb6b9b99d6e28b00e24d3548ed5
Thomas Tichy <ttichy@hotmail.de>
2024-03-23 11:39:11 +0100

    backup

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -17,7 +17,7 @@ export class HttpService {
   }
 
   putItem<T extends Game | Player>(item: Game | Player): Observable<HttpEvent<T>> {
-    const url: string = this.url + "time" in item ? "/games" : "/player"
+    const url: string = this.url + ("time" in item ? "/games" : "/player")
     console.log(url,item)
     return this.http
       .put<HttpEvent<T>>( url, item, this.httpOptions)
