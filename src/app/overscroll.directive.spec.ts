2e7a7d0578f61bfcd4fe855a97c9a8034ca44560
Thomas Tichy <ttichy@hotmail.de>
2024-03-26 16:48:03 +0100

    charts added

---
+++ src/app/overscroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { OverscrollDirective } from './overscroll.directive';
+
+describe('OverscrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new OverscrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/overscroll.directive.ts
@@ -0,0 +1,36 @@
+import { ElementRef, Directive, EventEmitter, HostListener, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { pullGamesHttp } from './store/game.action';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Directive({
+  selector: '[appOverscroll]',
+  standalone: true
+})
+export class OverscrollDirective {
+
+  constructor(private store: Store, private el: ElementRef) { }
+  tochstart = Infinity
+  @Output() loading = new EventEmitter(false)
+
+  @HostListener('touchstart', ['$event'])
+  scrollPostion(ev: Event) {
+    if (this.el.nativeElement instanceof HTMLDivElement) {
+      if (ev instanceof TouchEvent && this.el.nativeElement.scrollTop == 0) {
+        this.tochstart = ev.touches[0].clientY
+      }
+    }
+  }
+  @HostListener('touchmove', ['$event'])
+  scrollMove(ev: Event) {
+    if (ev instanceof TouchEvent) {
+      if (ev.touches[0].clientY - this.tochstart > 200) {
+        this.tochstart = Infinity
+        this.store.dispatch(pullGamesHttp({ start: 0, end: Date.now() }))
+        if (this.el.nativeElement instanceof HTMLDivElement) {
+          this.el.nativeElement.dispatchEvent(new Event('appOverscroll'))
+        }
+      }
+    }
+  }
+}
