9be63dee8ba44f466dd4b9832e70a71a0e1a0024
Thomas Tichy <ttichy@hotmail.de>
2024-03-26 18:45:26 +0100

    backup

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -28,6 +28,7 @@ export class TableComponent {
   @HostListener("window:orientationchange", ['$event'])
   orientationchange(event: Event) {
     this.isLandscape = screen.orientation && screen.orientation.angle !== 0;
+    console.log("orienttation change")
   }
 
   fun(ev: any) {
@@ -45,6 +46,9 @@ export class TableComponent {
     this.sum$ = this.store.select('sumTable')
     this.players$ = this.store.select('players')
     this.games$ = this.store.select('game')
+window.addEventListener('orientationchange', (event) => {
+  console.log('Orientation changed');
+});
   }
 
   ngOnInit() {
