89ff89ab8b782a78a0ff80dc2c140f67f99180b1
Thomas Tichy <ttichy@hotmail.de>
2026-02-09 13:35:45 +0100

    Auto-generated commit

---
+++ chunk-BEM2FJVF.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-STNK4T6T.js";import"./chunk-RG7DA2NH.js";export{a as UrlService};
--- chunk-MUEIOUQJ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-HOOBUMEV.js";import"./chunk-5QCQGMFG.js";export{a as UrlService};
